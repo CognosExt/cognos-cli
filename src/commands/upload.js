9fed7c4d9c1bf5aa2e1e98dcef182989ad89a3ae
batje <reinier@mountbatten.nl>
2020-05-28 10:48:32 +0200

    feat: allow for uploading visualisations

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -18,54 +18,76 @@ function getUpload(program) {
     )
     .option(
       '-e, --extname <extname>',
-      'The name of the extension or theme. Mandatory.'
+      'The name of the extension or theme or for Visualisations, the id. Mandatory.'
     )
     .option(
       '-c, --config [config]',
       'A .json configuration file. Any command-line parameter will override settings in this config file. The default is Settings.json.'
     )
-    .action(function(object, options) {
+    .action(function (object, options) {
       if (
         typeof object !== 'string' ||
-        (object !== 'theme' && object !== 'extension')
+        !(
+          object === 'theme' ||
+          object === 'extension' ||
+          object === 'visualisation'
+        )
       ) {
         console.error(
-          chalk.red('The object type must be one of "theme" or "extension"')
+          chalk.red(
+            'The object type must be one of "theme" or "extension" or "visualisation"'
+          )
         );
         process.exit(1);
       }
+      console.log(options.extname);
       var file = path.resolve(options.extname);
       var debug = options.debug !== undefined;
 
       var extname = options.extname !== undefined;
+      console.log(file);
 
-      var prom = getCognos(options, debug)
-        .then(function(mycognos) {
+      var prom = getCognos(options, true)
+        .then(function (mycognos) {
           if (object == 'extension') {
             mycognos
               .uploadExtension(file, extname, 'extentions', { sslcheck: false })
-              .then(function() {
+              .then(function () {
                 console.log('Uploaded Extension');
               })
-              .catch(function(err) {
+              .catch(function (err) {
                 console.log('Error uploading', err);
               });
           } else if (object == 'theme') {
             mycognos
               .uploadExtension(file, extname, 'themes', { sslcheck: false })
-              .then(function() {
+              .then(function () {
                 console.log('Uploaded Theme');
               })
-              .catch(function(err) {
+              .catch(function (err) {
+                console.log('Error uploading', err);
+              });
+          } else if (object == 'visualisation') {
+            console.log(options.extname);
+            extname = options.extname.slice(0, -4);
+            console.log(extname);
+            mycognos
+              .uploadExtension(file, extname, 'visualisation', {
+                sslcheck: false,
+              })
+              .then(function () {
+                console.log('Uploaded Visualisation');
+              })
+              .catch(function (err) {
                 console.log('Error uploading', err);
               });
           } else if (object == 'file') {
             mycognos
               .upLoadDataFile(file)
-              .then(function() {
+              .then(function () {
                 console.log('Uploaded Data File');
               })
-              .catch(function(err) {
+              .catch(function (err) {
                 console.log('Error uploading', err);
               });
           } else {
@@ -73,13 +95,13 @@ function getUpload(program) {
             process.exit(1);
           }
 
-          Promise.resolve(mycognos).then(function() {
+          Promise.resolve(mycognos).then(function () {
             console.log('Done uploading');
             process.exit(0);
           });
         })
-        .catch(function(err) {
-          console.log(chalk, red(err));
+        .catch(function (err) {
+          console.log(chalk.red(err));
           process.exit(1);
         });
       return prom;
